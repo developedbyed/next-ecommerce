c3e4391e9927be549de60d89f6d9ffaa63128515
developedbyed <ed.magician@gmail.com>
2023-04-02 14:35:20 +0100

    Changes for prod

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ["lh3.googleusercontent.com", "files.stripe.com"],
   },
